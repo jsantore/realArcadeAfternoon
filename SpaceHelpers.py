01f59b80b1922d0968b9c51cedb56447cb3f0917
jsantore <jsantore@bridgew.edu>
2023-11-28 14:37:44 -0500

    add rocks in random spots

--- SpaceHelpers.py
+++ SpaceHelpers.py
@@ -1,3 +1,22 @@
+import random
+
+import arcade
 
 WINDOW_WIDTH = 1500
 WINDOW_HEIGHT = 800
+
+def make_rocks():
+    rock_list = arcade.SpriteList()
+    possible_rocks = [
+        ":resources:images/space_shooter/meteorGrey_med1.png",
+        ":resources:images/space_shooter/meteorGrey_med2.png",
+        ":resources:images/space_shooter/meteorGrey_big4.png",
+        ":resources:images/space_shooter/meteorGrey_big2.png"
+    ]
+    for rock_number in range(10):
+        file_name = random.choice(possible_rocks)
+        rock  = arcade.Sprite(file_name)
+        rock.center_x = random.randint(150, WINDOW_WIDTH)
+        rock.center_y = random.randint(0, WINDOW_HEIGHT)
+        rock_list.append(rock)
+    return rock_list
--- SpaceWindow.py
+++ SpaceWindow.py
@@ -1,6 +1,7 @@
 import arcade
 from SpaceHelpers import WINDOW_WIDTH
 from SpaceHelpers import WINDOW_HEIGHT
+import SpaceHelpers
 class SpaceWindow(arcade.Window):
     def __init__(self):
         super().__init__(WINDOW_WIDTH, WINDOW_HEIGHT, "Space This time")
@@ -11,12 +12,14 @@ class SpaceWindow(arcade.Window):
         self.player = arcade.Sprite("Blue-05.png")
         self.player.center_x = 100
         self.player.center_y = WINDOW_HEIGHT/2
+        self.rocks = SpaceHelpers.make_rocks()
 
     def on_update(self, delta_time: float):
         pass
     def on_draw(self):
         arcade.start_render()
         self.player.draw()
+        self.rocks.draw()
         arcade.finish_render()
 
 def main():
